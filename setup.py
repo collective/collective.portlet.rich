be159e6881a283afc8b205881d45e23789739088
Wouter Vanden Hove <wouter.vanden.hove@gmail.com>
2015-01-06 20:34:16 +0100

    Back to development: 0.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = '0.5'
+version = '0.6.dev0'
 
 long_description = (
     read('README.rst')
